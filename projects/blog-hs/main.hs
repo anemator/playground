779437397cbacb573c5ffb6212c59291a45416aa
Robert Buonpastore <robert.buonpastore@gmail.com>
2022-06-12 15:20:05 -0400

    projects/blog-hs: 3.2 add type signatures and partial application

--- projects/blog-hs/main.hs
+++ projects/blog-hs/main.hs
@@ -1,10 +1,32 @@
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>" <> content <> "</title>"
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
+-- -> is right associative: a -> b -> c === a -> (b -> c)
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
 
 -- <> has right associativity: a <> (b <> c) === a <> b <> c
 -- . is function compositiion: (f . g) x === f(g(x))
+makeHtml :: String -> String -> String
 makeHtml title content = html_ $ (head_ . title_ $ title) <> body_ content
 
-main = putStrLn $ makeHtml "My page title" "My page content"
+myHtml :: String
+myHtml = makeHtml "My page title" (h1_ "My page header" <> p_ "My page content")
+
+main = putStrLn myHtml
