0c912e358bec1a495c8010290ac38fbb7a7c0fa4
Robert Buonpastore <robert.buonpastore@gmail.com>
2022-06-12 16:55:24 -0400

    projects/blog-hs: 3.5 move html functions into Html module

---
+++ projects/blog-hs/Html.hs
@@ -0,0 +1,47 @@
+module Html
+  ( Html -- export type only NOT constructor
+  , Title
+  , Structure
+  , html_
+  , p_
+  , h1_
+  , append_
+  , render
+  )
+  where
+
+-- left Html: type-name lives in types namespace
+-- right Html: constructor lives in the expression namespace
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+-- type alias
+type Title = String
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure lhs) (Structure rhs) = Structure $ lhs <> rhs
+
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
+
+-- -> is right associative: a -> b -> c === a -> (b -> c)
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
+
+-- <> has right associativity: a <> (b <> c) === a <> b <> c
+-- . is function compositiion: (f . g) x === f(g(x))
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html $ el "html" $
+      (el "head" $ el "title" title) <> (el "body" $ getStructureString content)
+
+render :: Html -> String
+render (Html str) = str
--- projects/blog-hs/main.hs
+++ projects/blog-hs/main.hs
@@ -1,34 +1,4 @@
--- left Html: type-name lives in types namespace
--- right Html: constructor lives in the expression namespace
-newtype Html = Html String
-newtype Structure = Structure String
-
--- type alias
-type Title = String
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure lhs) (Structure rhs) = Structure $ lhs <> rhs
-
-getStructureString :: Structure -> String
-getStructureString (Structure str) = str
-
--- -> is right associative: a -> b -> c === a -> (b -> c)
-el :: String -> String -> String
-el tag content =
-  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
--- <> has right associativity: a <> (b <> c) === a <> b <> c
--- . is function compositiion: (f . g) x === f(g(x))
-html_ :: Title -> Structure -> Html
-html_ title content =
-  Html $ el "html" $
-      (el "head" $ el "title" title) <> (el "body" $ getStructureString content)
+import Html
 
 myHtml :: Html
 myHtml =
@@ -36,7 +6,5 @@ myHtml =
     "My page title"
     (append_ (h1_ "My page header") (p_ "My page content"))
 
-render :: Html -> String
-render (Html str) = str
-
+main :: IO ()
 main = putStrLn $ render myHtml
