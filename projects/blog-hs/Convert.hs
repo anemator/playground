a30df48a1a5b9f61cb82779dbe295899fc1a9236
Robert Buonpastore <robert.buonpastore@gmail.com>
2022-06-12 22:29:00 -0400

    projects/blog-hs: 5.1 add convert function

--- projects/blog-hs/Convert.hs
+++ projects/blog-hs/Convert.hs
@@ -4,6 +4,9 @@ module Convert where
 import qualified Html
 import qualified Markup
 
+convert :: Html.Title -> Markup.Document -> Html.Html
+convert title = Html.html_ title . foldMap convertStructure
+
 convertStructure :: Markup.Structure -> Html.Structure
 convertStructure structure =
   case structure of
